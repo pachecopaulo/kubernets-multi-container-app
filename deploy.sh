7b3c3c7e4931b347765b11be8e0a122d69d1c857
Paulo Eduardo Pacheco <eduardoguitar@gmail.com>
2019-02-03 03:57:34 +0100

    push latest image

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
-docker build -t pachecopaulo/multi-client:latest -t pachecopaulo/multi-client:$SHA ./client
-docker build -t pachecopaulo/multi-server:latest -t pachecopaulo/multi-server:$SHA .
+docker build pachecopaulo/multi-client:latest
+docker build pachecopaulo/multi-server:latest
 
 docker push pachecopaulo/multi-client:latest
 docker push pachecopaulo/multi-server:latest
 
-docker push pachecopaulo/multi-client:$SHA
-docker push pachecopaulo/multi-server:$SHA
-
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=pachecopaulo/multi-server:$SHA
-kubectl set image deployments/client-deployment client=pachecopaulo/multi-client:$SHA
+#docker build -t pachecopaulo/multi-client:latest -t pachecopaulo/multi-client:$SHA ./client
+#docker build -t pachecopaulo/multi-server:latest -t pachecopaulo/multi-server:$SHA .
+#
+#docker push pachecopaulo/multi-client:latest
+#docker push pachecopaulo/multi-server:latest
+#
+#docker push pachecopaulo/multi-client:$SHA
+#docker push pachecopaulo/multi-server:$SHA
+#
+#kubectl apply -f k8s
+#kubectl set image deployments/server-deployment server=pachecopaulo/multi-server:$SHA
+#kubectl set image deployments/client-deployment client=pachecopaulo/multi-client:$SHA
